e4f7d17733f05521e3c6825d4dc337c24327015d
pdrvstv <burnsmoneys@gmail.com>
2026-09-15 20:29:50 +0400

    Install approved 3D menu icons with static CSS mapping

---
+++ assets/menu-icons-v2-loader.js
@@ -0,0 +1,9 @@
+(()=>{'use strict';
+const id='ptg-menu-icons-v2-css';
+if(document.getElementById(id))return;
+const link=document.createElement('link');
+link.id=id;
+link.rel='stylesheet';
+link.href='/assets/menu-icons-v2.css?v=20260915-2015';
+document.head.appendChild(link);
+})();
