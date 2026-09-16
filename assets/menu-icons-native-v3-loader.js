2be49cd541f3f64f8784859739d2ebb6d9dce83b
pdrvstv <burnsmoneys@gmail.com>
2026-09-16 08:53:44 +0400

    Replace second-screen menu icons with approved WebP assets

---
+++ assets/menu-icons-native-v3-loader.js
@@ -0,0 +1,9 @@
+(()=>{'use strict';
+const id='ptg-menu-icons-native-v3-css';
+if(document.getElementById(id))return;
+const link=document.createElement('link');
+link.id=id;
+link.rel='stylesheet';
+link.href='/assets/menu-icons-native-v3.css?v=20260916-0846';
+document.head.appendChild(link);
+})();
