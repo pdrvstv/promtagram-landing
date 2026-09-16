418f5fbe217d9b4da9b91db54cdb7f928e6ca675
pdrvstv <burnsmoneys@gmail.com>
2026-09-16 11:29:49 +0400

    Add stats typography v2 loader

---
+++ assets/stats-editorial-v2-loader.js
@@ -0,0 +1,9 @@
+(()=>{'use strict';
+const id='ptg-stats-editorial-v2-css';
+if(document.getElementById(id))return;
+const link=document.createElement('link');
+link.id=id;
+link.rel='stylesheet';
+link.href='/assets/stats-editorial-v2.css?v=20260916-1130';
+document.head.appendChild(link);
+})();
