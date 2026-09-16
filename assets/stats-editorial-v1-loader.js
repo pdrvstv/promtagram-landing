131b6582cbaa94a250640fcdbbab693092be9541
pdrvstv <burnsmoneys@gmail.com>
2026-09-16 11:23:56 +0400

    Add stats editorial stylesheet loader

---
+++ assets/stats-editorial-v1-loader.js
@@ -0,0 +1,9 @@
+(()=>{'use strict';
+const id='ptg-stats-editorial-v1-css';
+if(document.getElementById(id))return;
+const link=document.createElement('link');
+link.id=id;
+link.rel='stylesheet';
+link.href='/assets/stats-editorial-v1.css?v=20260916-1121';
+document.head.appendChild(link);
+})();
